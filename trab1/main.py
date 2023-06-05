2716c30bdb6b98f244699289fd7de5933c1af1b9
alexzarp <alexszds@gmail.com>
2023-06-05 10:41:04 -0300

    #

--- trab1/main.py
+++ trab1/main.py
@@ -1,13 +1,47 @@
+def print_matrix(matrix, msg = ''):
+    if msg !='':
+        print(msg)
+    for row in matrix:
+        print(row)
+
+def print_matrix_beautiful(matrix, msg = ''):
+    if msg !='':
+        print(msg)
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Determinar a largura máxima para cada coluna
+    column_widths = [max(len("{:.4f}".format(matrix[i][j])) if matrix[i][j] is not None else 0 for i in range(rows)) for j in range(cols)]
+
+    # Imprimir a matrix com caracteres de moldura
+    for i in range(rows):
+        # Imprimir linha superior da célula
+        line = "+" + "+".join("-" * (width + 2) for width in column_widths) + "+"
+        print(line)
+
+        # Imprimir conteúdo da célula
+        for j in range(cols):
+            if matrix[i][j] is not None:
+                cell = "| {:{width}.4f} ".format(matrix[i][j], width=column_widths[j])
+            else:
+                cell = "| {:{width}} ".format("", width=column_widths[j])
+            print(cell, end="")
+        print("|")
+
+    # Imprimir linha inferior da célula
+    line = "+" + "+".join("-" * (width + 2) for width in column_widths) + "+"
+    print(line)
+
 def calculate_tx_points(plate):
     rows = len(plate)
     cols = len(plate[0])
 
-    # Criar uma cópia da matriz 'plate' para armazenar os resultados
-    result = [[plate[i][j] for j in range(cols)] for i in range(rows)]
+    # Criar uma cópia da matrix 'plate' para armazenar os resultados
+    # result = [[plate[i][j] for j in range(cols)] for i in range(rows)]
 
     for i in range(rows):
         for j in range(cols):
-            if isinstance(plate[i][j], str) and plate[i][j].startswith('t'.lower()):
+            if isinstance(plate[i][j], str) and plate[i][j].startswith('t'):
                 sum_neighbors = 0
                 count_neighbors = 0
 
@@ -26,11 +60,11 @@ def calculate_tx_points(plate):
                     count_neighbors += 1
 
                 if count_neighbors > 0:
-                    result[i][j] = sum_neighbors / count_neighbors
+                    plate[i][j] = sum_neighbors / count_neighbors
                 else:
-                    result[i][j] = None
+                    plate[i][j] = None
 
-    return result
+    return plate
 
 
 # Exemplo de uso
@@ -43,7 +77,7 @@ plate = [
 ]
 
 tx_points = calculate_tx_points(plate)
+tx_points = calculate_tx_points(tx_points)
 
-print("Tx Points:")
-for row in tx_points:
-    print(row)
+print_matrix(tx_points, "Tx Points:")
+print_matrix_beautiful(tx_points,"Tx Points:")
